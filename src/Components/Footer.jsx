92aaaf603df92c8770624f3af073cb79b4e6f9a1
caio-cmc <caiocesarmcruz@gmail.com>
2023-06-28 17:02:49 -0300

    footer stylized

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import "../Styles/Components/Footer.scss"
 
 function Footer() {
   return (
-    <footer>
-      <span>Feito por <a href="https://github.com/caio-cmc" target="_blank" rel="noreferrer">Caio César</a> em 2023</span>
+    <footer className="footer-main">
+      <p>
+        Feito por
+          <a className="footer-caio" href="https://github.com/caio-cmc" target="_blank" rel="noreferrer">
+            <b> Caio César</b>
+          </a> em 2023
+      </p>
+      <p className="footer-disclaimer"><b>Atenção: </b>A WheyCulator não está aqui para dizer qual a melhor marca de whey do mercado, apenas para comparar as quantidades de proteína em suas composições. Procure sempre a opinião de um profissional qualificado.</p>
     </footer>
   )
 }
